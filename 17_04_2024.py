1fafb1e31018ef6118e00d32935a75c873749f6e
User <luis.ott@edu.bib.de>
2024-04-17 12:58:54 +0200

    initial

---
+++ 17_04_2024.py
@@ -0,0 +1,24 @@
+for i in range(10, 0, -1):
+    print(i)
+liste = [10, 11, 0, 3]
+for i in liste:
+    print(i)
+print(range(5))
+x=101
+while x<100:
+    print(test)
+t = "0"
+t = int(t)
+t = 0
+t = str(t)
+
+minuten = input("Geben Sie die Zahlen in Minuten an: ")
+Stunden = minuten/60 - (minuten%60)
+minuten = minuten%60
+sekunden = minuten%1
+minuten = minuten - sekunden
+sekunden = sekunden*60
+sekunden = int(sekunden)
+print(stunden, minuten, sekunden)
+raten = input()
+input()
